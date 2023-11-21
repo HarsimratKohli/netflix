611b8e38e75ac04506718e4389d04f1a22d03eb9
harsimrat <harsimratsingh_1997@hotmail.com>
2023-11-21 20:13:06 +0530

    Google - Github OAuth

--- components/input.tsx
+++ components/input.tsx
@@ -10,14 +10,15 @@ interface InputProps{
 
 const Input: React.FC<InputProps> = ({id, onChange, value, label, type}) =>{
     return (
-        <div className=" relative">
-            <input id={id}
-            type={type}
-            value={value}
-            onChange={onChange}
-            className="
-                block rounder-md px-6 pt-6 pb-1 w-full text-md text-white bg-neutral-700 appearance-none focus:outline-none focus:ring-0 peer"
-            placeholder=" "
+        <div className="relative">
+            <input
+                id={id}
+                type={type}
+                value={value}
+                onChange={onChange}
+                className="
+                    block rounded-md px-6 pt-6 pb-1 w-full text-md text-white bg-neutral-700 appearance-none focus:outline-none focus:ring-0 peer"
+                placeholder=" "
             />
             <label className="
              absolute
@@ -32,10 +33,10 @@ const Input: React.FC<InputProps> = ({id, onChange, value, label, type}) =>{
              origin-[0]
              left-6
              peer-placeholder-shown:scale-100
-             peer-placeholder-shown: translate-y-0
+             peer-placeholder-shown:translate-y-0
              peer-focus:scale-75
              peer-focus:-translate-y-3" 
-            htmlFor={id}>
+             htmlFor={id}>
                 {label}
             </label>
         </div>
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -3,9 +3,21 @@ import Credentials from "next-auth/providers/credentials";
 import prismadb from '@/lib/prismadb';
 import { defaultConfig } from "next/dist/server/config-shared";
 import { compare } from 'bcrypt';
+import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+
 
 export default NextAuth({
   providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID || "",
+      clientSecret: process.env.GITHUB_SECRET || ""
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || ""
+    }),
     Credentials({
       id: 'credentials',
       name: 'Credentials',
@@ -47,9 +59,10 @@ export default NextAuth({
     signIn: '/auth',
   },
   debug: process.env.NODE_ENV === 'development',
+  adapter: PrismaAdapter(prismadb),
   session: { strategy: 'jwt' },
   jwt: {
     secret: process.env.NEXTAUTH_JWT_SECRET,
   },
-  secret: process.env.NEXTAUTH_SECRET
+  secret: process.env.NEXTAUTH_SECRET,
 })
