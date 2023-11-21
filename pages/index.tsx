a4cb24c5d41dfb10f0de8c6b451b91ce3c872daa
harsimrat <harsimratsingh_1997@hotmail.com>
2023-11-21 22:35:51 +0530

    Profile screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,34 @@
-import Auth from "./auth"
+import { getSession, signOut } from "next-auth/react"
+import { NextPageContext } from "next"
+import useCurrentUser from "@/hooks/useCurrentUser";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+  
+  if(!session){
+    return {
+      redirect:{
+        destination: '/auth',
+        permanent: false
+      }
+    }
+  }
+
+  return {
+    props:{}
+  }
+}
+
 export default function Home() {
+
+  const { data: user} = useCurrentUser();
+
+
   return (
     <>
-      <p className="color text-white">Netflix Clone</p>
+      <p className="color text-green-500 text-5xl">Netflix Clone</p>
+      <p className="text-white"> Logged in as: { user?.name }</p>
+      <button className="h-10 w-full bg-red-500" onClick={() => signOut()}>Logout!</button>
     </>
   )
 }
